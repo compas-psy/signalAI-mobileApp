225c61fc6ecfe184e53988b3ee1fe62ddda9d786
compas-psy <eliah.n.martynov@gmail.com>
2026-08-15 02:06:00 +0300

    feat: add strategy measurement package

---
+++ server/app/measurement/__init__.py
@@ -0,0 +1,13 @@
+"""Deterministic strategy measurement and comparison helpers."""
+
+from .report import (
+    MeasurementDataset,
+    StrategyMeasurementRecord,
+    build_strategy_measurement_report,
+)
+
+__all__ = [
+    "MeasurementDataset",
+    "StrategyMeasurementRecord",
+    "build_strategy_measurement_report",
+]
